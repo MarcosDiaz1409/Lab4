f32af178d9a178cd9119b06cf6dc3e253d5c0f3b
Diego Soto <sot22737@uvg.edu.gt>
2022-11-08 14:24:16 -0600

    Clase funcionalidades

---
+++ Funcionalidades.java
@@ -0,0 +1,19 @@
+public interface Funcionalidades {
+    public String viajes();
+    public String encenderApagar();
+    public String contactos();
+    public String llamar();
+    public String finalizarLLamada();
+    public String speaker();
+    public String listaReproduccion(int i);
+    public String cancion();
+    public String escuchar();
+    public String cambiarModo();
+    public String emisoras();
+    public void guardarEmisoras();
+    public String cargarEmisora();
+    public int bajarVolumen();
+    public int subirVolumen();
+    public boolean fswitch();
+    public String estado();   
+}
