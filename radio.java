d37353e0b39588c18bc96b9dd151e12d17120df5
Diego Soto <sot22737@uvg.edu.gt>
2022-11-11 13:45:29 -0600

    Parte de mis metodos

--- radio.java
+++ radio.java
@@ -6,8 +6,8 @@ public class radio implements Funcionalidades{
     private String emisora;
     private ArrayList<String> emisoras;
     private ArrayList<listas> albumes;
-    private ArrayList<String> contactos;
-
+    private String contactos[] = {"Luis Pedro...12345678", "Sebastian Perez...87654321", "Mercedes del Valle...42344289"};
+    private boolean speaker;
 
     public radio() {
         banda = "";
@@ -15,17 +15,16 @@ public class radio implements Funcionalidades{
         emisora = "";
         emisoras = new ArrayList<String>();
         albumes = new ArrayList<listas>();
-        contactos = new ArrayList<String>();
+        speaker = false;
     }
 
-
-    public radio(String banda, String frecuencia, String emisora, ArrayList<String> emisoras, ArrayList<listas> albumes, ArrayList<String> contactos) {
+    public radio(String banda, String frecuencia, String emisora, ArrayList<String> emisoras, ArrayList<listas> albumes, boolean speaker) {
         this.banda = banda;
         this.frecuencia = frecuencia;
         this.emisora = emisora;
         this.emisoras = emisoras;
         this.albumes = albumes;
-        this.contactos = contactos;
+        this.speaker = speaker;
     }
 
 
@@ -67,14 +66,6 @@ public class radio implements Funcionalidades{
 
     public void setAlbumes(ArrayList<listas> albumes) {
         this.albumes = albumes;
-    }
-
-    public ArrayList<String> getContactos() {
-        return this.contactos;
-    }
-
-    public void setContactos(ArrayList<String> contactos) {
-        this.contactos = contactos;
     }
 
 
@@ -167,7 +158,8 @@ public class radio implements Funcionalidades{
     @Override
     public String llamarContactos(String numero) {
         // TODO Auto-generated method stub
-        return null;
+        String resultado = "Se esta llamando al numero: "+numero+" de su lista de contactos";
+        return resultado;
     }
 
 
@@ -181,14 +173,21 @@ public class radio implements Funcionalidades{
     @Override
     public boolean CambiarSpeaker() {
         // TODO Auto-generated method stub
-        return false;
+        if (speaker){
+            speaker = false;
+        }
+        else{
+            speaker = true;
+        }
+        return speaker;
     }
 
 
     @Override
     public String PlanificarViajes(String destino) {
         // TODO Auto-generated method stub
-        return null;
+        String resultado = "Se ha planificado su viaje a "+destino;
+        return resultado;
     }
 
     
